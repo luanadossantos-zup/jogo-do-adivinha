efde8f675ea3910bde69fc3bd413fbc7a24e8a64
Luana Silva <luana.dossantos@zup.com.br>
2024-12-12 13:54:19 -0300

    feat: adiciona método Jogando o Jogo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,7 +6,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int nivelDificuldade = 0;
         int numeroAleatorio = 0;
-        int numeroDeRodadas = 0;
+
 
 
         System.out.println("-------------------------------------");
@@ -14,14 +15,15 @@ public class Main {
         System.out.println(" ");
 
 
-        numeroAleatorio = jogadorSelecionaDificuldade(scanner, nivelDificuldade,numeroAleatorio);
-        numeroDeRodadas = numeroDeRodadas(scanner, numeroDeRodadas);
+        jogandoOJogo(scanner, numeroAleatorio);
 
 
     }
 
-    public static int jogadorSelecionaDificuldade (Scanner scanner, int nivelDificuldade,int numeroAleatorio) {
-        System.out.println("Digite um número para selecionar a dificuldade:");
+    public static int jogadorSelecionaDificuldadeRetornaRandom (Scanner scanner, int numeroAleatorio) {
+        int nivelDificuldade = 0;
+
+        System.out.println("Por favor, digite um número para selecionar a dificuldade:");
 
 
         do {
@@ -75,24 +77,55 @@ public class Main {
         return numeroAleatorio;
     }
 
-    public static int numeroDeRodadas(Scanner scanner, int numeroDeRodadas) {
-        System.out.println("Digite o número de rodadas (de 1 a 15:");
+    public static void jogandoOJogo(Scanner scanner, int numeroAleatorio) {
+        ArrayList<Integer> acertos = new ArrayList<>();
+        ArrayList<Integer> erros = new ArrayList<>();
+        int pontuacao = 0;
+        int numeroEscolhido;
+
+        String continuarJogo ="";
+
+
 
         do {
+            numeroAleatorio = jogadorSelecionaDificuldadeRetornaRandom(scanner, numeroAleatorio);
+
             try {
-                numeroDeRodadas = scanner.nextInt();
+                System.out.println("Faça seu palpite. Digite um número:");
+                numeroEscolhido = scanner.nextInt();
+
+                if (numeroEscolhido == numeroAleatorio) {
+                    System.out.println("Parabéns, você acertou o número");
+                    pontuacao = pontuacao + 10;
+                    acertos.add(numeroEscolhido);
+
+                } else if (numeroEscolhido == numeroAleatorio +1 || numeroEscolhido == numeroAleatorio -1) {
+                    System.out.println("Quase lá! Você estava a 1 de distância do número sorteado. Você ganhou 5 pontos.");
+                    pontuacao = pontuacao +5;
+                    erros.add(numeroAleatorio);
+                } else {
+                    System.out.println("Infelizmente você não acertou e não ganhou nenhum ponto!");
+                    erros.add(numeroAleatorio);
+                }
             } catch (Exception e) {
                 System.out.println("-------------------------------------");
-                System.out.println("Por favor, apenas números de 1 a 15!");
+                System.out.println("Por favor, digite apenas números!");
                 System.out.println("-------------------------------------");
                 scanner.next();
-
             }
 
+            System.out.println("Continuar jogo? Digite S para Sim ou qualquer tecla para não:");
+            continuarJogo = scanner.next().toLowerCase();
 
+        } while (continuarJogo.equals("s"));
 
-        } while(numeroDeRodadas > 15 || numeroDeRodadas <1);
+        System.out.println("Pontuação final " + pontuacao + " pontos.");
+        System.out.println("Números acertados: " + acertos);
+        System.out.println("Números errados: " + erros);
 
-        return numeroDeRodadas;
+        System.exit(0);
+
     }
+
+
 }
